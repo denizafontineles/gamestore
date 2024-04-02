299810b55af3f41fccae3eba7c0239f2adc6a014
denizafontineles <denizafontineles@outlook.com>
2024-04-02 18:59:51 -0300

    adiciona CRUD de Produtos e Categorias da loja de jogos

---
+++ src/main/java/com/generation/gamestore/model/Categoria.java
@@ -0,0 +1,61 @@
+package com.generation.gamestore.model;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo Tipo é Obrigatório")
+	@Size(min = 2, max = 200, message = "Tamanho mínimo: 2, Tamano máximo: 200")
+	private String tipo;
+	
+	//1 categoria pode ter vários produtos N (1:N)
+	//Como a Classe Categoria será a Classe Proprietária da Relação, precisamos adicionar alguns parâmetros:
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "categoria", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("categoria")
+	private List<Produto> produto; //receberá todos os Objetos da Classe Produto associadas a cada Objeto da Classe Categoria.
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
+	}
+	
+	
+}
---
+++ src/main/java/com/generation/gamestore/model/Produto.java
@@ -0,0 +1,128 @@
+package com.generation.gamestore.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo Nome é Obrigatório")
+	@Size(min = 2, max = 200, message = "Tamanho mínimo: 2, Tamano máximo: 200")
+	private String nome;
+	
+	@NotBlank(message = "O atributo Descrição é Obrigatório")
+	@Size(min = 5, max = 1000, message = "Tamanho mínimo: 5, Tamano máximo: 1000")
+	private String descricao;
+	
+	@NotBlank(message = "O atributo Console é Obrigatório")
+	@Size(min = 2, max = 200, message = "Tamanho mínimo: 2, Tamano máximo: 200")
+	private String console;
+	
+	@NotNull(message = "O Atributo Quantidade é obrigatório")
+	private int quantidade;
+
+	@NotNull(message = "O Atributo Preço é obrigatório")
+	@Column(precision = 8, scale = 2) // DECIMAL(8,2) 
+	private BigDecimal preco; 
+
+	private String foto;
+	
+	@UpdateTimestamp
+	private LocalDateTime data;
+	
+	@ManyToOne // vários produtos N pode pertencer a 1 categoria (N:1)
+	@JsonIgnoreProperties("produto") 
+	private Categoria categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getConsole() {
+		return console;
+	}
+
+	public void setConsole(String console) {
+		this.console = console;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public LocalDateTime getData() {
+		return data;
+	}
+
+	public void setData(LocalDateTime data) {
+		this.data = data;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+}
